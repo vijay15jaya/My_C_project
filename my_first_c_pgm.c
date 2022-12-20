77485bd5e7b6675524aab7d2a2eebe88c5060985
vzzzjzzz <vijayajaya1003@gmail.com>
2022-12-21 01:00:27 +0530

    [rs][jira]updated c pgm

--- my_first_c_pgm.c
+++ my_first_c_pgm.c
@@ -4,5 +4,6 @@ int main(void)
    printf ("Hello from your first program!\n");
    printf ("This is to test build trigger options in jenkins");
    printf("This is 3rd line");
+   printf("This 4th line");
    return 0;
 }
